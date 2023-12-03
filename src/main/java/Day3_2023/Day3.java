8168a64d8187b8353557db07f6dcb7b5e97647d4
JeffW12345 <59927061+JeffW12345@users.noreply.github.com>
2023-12-03 14:45:28 +0000

    Update Day3.java
    Made static final variable all capitals.

--- src/main/java/Day3_2023/Day3.java
+++ src/main/java/Day3_2023/Day3.java
@@ -5,18 +5,18 @@ import java.util.Scanner;
 
 public class Day3 {
     private static final File FILE = new File("src/main/resources/engine.txt");
-    private static final Grid grid = new Grid();
+    private static final Grid GRID = new Grid();
 
     public static void main(String[] args) {
         importData();
-        NumbersInGrid numbersInGrid = new NumbersInGrid(grid);
+        NumbersInGrid numbersInGrid = new NumbersInGrid(GRID);
         System.out.println("Output: " + numbersInGrid.getTotal());
     }
 
     private static void importData() {
         try (Scanner scanner = new Scanner(FILE)) {
             while (scanner.hasNextLine()) {
-                grid.addRow(scanner.nextLine());
+                GRID.addRow(scanner.nextLine());
             }
         } catch (Exception e) {
             System.out.println("An error occurred.");
